87e92f774a267d26ba75177a8792fadcd636afcd
Pavan <pavansgowda6@gmail.com>
2023-08-01 14:07:10 +0530

    javascript personal portfolio first commit

---
+++ js/main_script.js
@@ -0,0 +1,217 @@
+/* ========================= SHOW MENU ================================= */
+
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/* ======== MENU SHOW ======== */
+/* Validate if constant exists */
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* ======== MENU HIDDEN ======== */
+/* Validate if constant exists */
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/* ========================= REMOVE MENU MOBILE ============================ */
+
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+
+  // When we click on each nav_link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/* 
+This code (navLink.forEach(n => n.addEventListener('click', linkAction))
+uses the forEach() method to loop through each element in the navLink NodeList and adds an event listener to each element.
+
+The event listener is set to listen for a click event and calls the linkAction function when the event occurs. 
+
+This ensures that when a navLink element is clicked, the linkAction function is executed and the show-menu class is removed from the navMenu element.)
+*/
+
+/* ======================== CHANGE BACKGROUND HEADER ================= */
+const scrollHeader = () =>{
+  const header = document.getElementById('header')
+  // when the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+
+  this.scrollY >=50? header.classList.add('bg-header')
+                   : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/* ======================== SWIPER PROJECTS ================================= */
+
+let swiperProjects = new Swiper(".projects__container", {
+  loop: true,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: -56,
+    },
+  },
+});
+
+/* ================== EMAIL JS =========================== */
+
+const contactForm = document.getElementById("contact-form"),
+  contactName = document.getElementById("contact-name"),
+  contactEmail = document.getElementById("contact-email"),
+  userMessage = document.getElementById("user-message"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  //Check if the field has a value
+  if (
+    contactName.value === "" ||
+    contactEmail.value === "" ||
+    userMessage.value === "" 
+  ) {
+   
+    
+    //Add or remove color
+    contactMessage.classList.remove("color-blue");
+    contactMessage.classList.add("color-red");
+
+     // Show message
+     contactMessage.textContent = "Please fill all the input fields";
+  
+    
+  }else{
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_l4hg5nt','template_l2dx3vo','#contact-form','P3w7jqf8sRiVm34VI')
+    
+    .then(()=>{
+        // show message and add color
+        contactMessage.classList.add('color-blue')
+        contactMessage.textContent = 'Message sent ✅'
+
+        // Remove message after five seconds
+        setTimeout(()=>{
+            contactMessage.textContent = ''
+        },5000)
+    },(error) => {
+      alert('OOPS! Something went wrong..', error)
+    })
+    // To clear the input field
+    contactName.value = ''
+    contactEmail.value = ''
+    userMessage.value = ''
+  }
+};
+contactForm.addEventListener("submit", sendEmail)
+
+/* ================== SCROLL SECTION ACTIVE LINK =========================== */
+
+const sections = document.querySelectorAll('.section[id]')
+const scrollActive = () => {
+
+const scrollY = window.pageYOffset
+sections.forEach(current => {
+  const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetHeight - 58,
+        sectionId = current.getAttribute('id'),
+      sectionClass = document.querySelector('.nav__menu a[href*='+ sectionId +']')
+
+  if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+    sectionClass.classList.add('active-link')
+  }else{
+    sectionClass.classList.remove('active-link')
+  }
+})
+}
+window.addEventListener('scroll',scrollActive)
+
+/*==================== SHOW SCROLL UP ============================*/
+
+const scrollUp = () =>{
+  const scrollUp = document.getElementById('scroll-up')
+  // when the scroll is higher than the 350 viewport height, add the show-scroll class to the 'a' tag with the scroll up
+
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
+
+/*==================== DARK LIGHT THEME ============================*/
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line' 
+
+// Previously selected topic (if user selected)
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+const getCurrentTheme = () =>{
+  document.body.classList.contains(darkTheme)? 'dark' : 'light'
+}
+
+const getCurrentIcon= () =>{
+  themeButton.classList.contains(iconTheme)? 'ri-moon-line' : 'ri-sun-line'
+}
+
+// We validate if the user previously chose a topic
+
+if(selectedTheme){
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+
+document.body.classList[selectedTheme === 'dark'? 'add' : 'remove'](darkTheme)
+themeButton.classList[selectedIcon === 'ri-moon-line'?'add' : 'remove'](iconTheme)
+}
+
+//Activate / deactivate the theme manually with the button
+
+themeButton.addEventListener('click', () =>{
+
+// Add or remove the dark / icon theme
+document.body.classList.toggle(darkTheme)
+themeButton.classList.toggle(iconTheme)
+
+// we save the theme and the current icon that the user choose
+
+localStorage.setItem('selected-theme',getCurrentTheme())
+localStorage.setItem('selected-icon',getCurrentIcon())
+})
+
+/* ================ SCROLL REVEAL ANIMATION =============================*/
+
+const sr = ScrollReveal({
+  origin   : 'top',
+  distance : '60px',
+  duration : 2500,
+  delay    :400
+  // reset : true  /* animations repeat */
+})
+
+sr.reveal(`.home__data , .projects__container, .footer__container`)
+sr.reveal(`.home__info div`, {delay:600, origin: 'bottom', interval:100})
+sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, { origin: 'left'})
+sr.reveal(`.contact__content:nth-child(2)`, { origin: 'right'})
+sr.reveal(`.qualification__content, .services__card`, { interval:100})
